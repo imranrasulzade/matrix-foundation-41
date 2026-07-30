6a4f08793a6a1063ee4dbb69196c7c46814c956f
imranrasulzade <imranrasulzade5@gmail.com>
2026-07-30 20:14:53 +0400

    some line writed

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,16 @@ public class Main {
         System.out.println(85 + 52);
         System.out.println("Hello");
 //        System.out.println("Salam");
+//        System.out.println("Salam");
+//        System.out.println("Salam");
+//        System.out.println("Salam");
+//        System.out.println("Salam");
+//        System.out.println("Salam");
+//        System.out.println("Salam");
+//        System.out.println("Salam");
+//        System.out.println("Salam");
+//        System.out.println("Salam");
+//        System.out.println("Salam");
 
 
     }
