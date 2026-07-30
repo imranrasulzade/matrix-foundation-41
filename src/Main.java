7e65d035d3998970b97cf4bc0ff1ef01bfa2a5b6
imranrasulzade <imranrasulzade5@gmail.com>
2026-07-30 20:36:08 +0400

    for practice

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,7 @@ public class Main {
         // salam capa verir bu kod
         System.out.println("Hello");
         System.out.println(85 + 52);
-        System.out.println("Hello");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
-//        System.out.println("Salam");
+        System.out.println("Minaye");
 
 
     }
