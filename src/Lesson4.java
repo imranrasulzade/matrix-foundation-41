896278c5f5952ec7ddedc6920791a7f8eb0f24f7
imranrasulzade <imranrasulzade5@gmail.com>
2026-08-01 17:45:43 +0400

    calculator code add

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -27,7 +27,7 @@ public class Lesson4 {
 
 //        System.out.println("Proqram bitdi");
 
- // vurma cedveli
+        // vurma cedveli
 //        for (int i = 1; i <= 10; i++) {
 //            for (int j = 1; j <= 10; j++) {
 //                System.out.print(j + " x " + i + " = " + (i * j) + "\t\t");
@@ -69,7 +69,7 @@ public class Lesson4 {
 //        System.out.println(sum);
 
 
- //              1 2 3
+        //              1 2 3
         // 0 1 1 2 3 5 8 13 21 34 55
 //                 1 2 3
 //        int birinci  = 0;
@@ -82,15 +82,50 @@ public class Lesson4 {
 //        }
 
 
+        Scanner sc = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            System.out.println("""
+                    1. Topla
+                    2. Cix
+                    3. Vur
+                    4. Bol
+                    5. Exit
+                    """);
+            int menu = sc.nextInt();
 
+            switch (menu) {
+                case 1:
+                    int a = sc.nextInt();
+                    int b = sc.nextInt();
+                    System.out.println(a + b);
+                    break;
+                case 2:
+                    int c = sc.nextInt();
+                    int d = sc.nextInt();
+                    System.out.println(c - d);
+                    break;
+                case 3:
+                    int e = sc.nextInt();
+                    int f = sc.nextInt();
+                    System.out.println(e * f);
+                    break;
+                case 4:
+                    int g = sc.nextInt();
+                    int h = sc.nextInt();
+                    System.out.println(g / h);
+                    break;
+                case 5:
+                    running = false;
+//                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid menu");
 
+            }
+        }
 
-
-
-
-
-
-
+        System.out.println("Salam");
 
 
     }
