defe8574ec525834c59a8ed56dc338b848736065
imranrasulzade <imranrasulzade5@gmail.com>
2026-08-01 18:56:38 +0400

    lesson5

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -2,6 +2,88 @@ public class Lesson5 {
 
 
     public static void main(String[] args) {
+//        String a = "Gulnur";
+//        String b = "Fidan";
+//        String c = "Minaye";
+//        String d = "Fatime";
+//
+//        int m = 5;
+//        String[] names = new String[4];
+//        names[0] = "Gulnur";
+//        names[1] = "Fidan";
+//        names[2] = "Minaye";
+//        names[3] = "Fatime";
+//        names[4] = "Fatime";
+//
+//
+////        System.out.println(names[0]);
+////        System.out.println(names[1]);
+////        System.out.println(names[2]);
+////        System.out.println(names[3]);
+//
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println(names[i]);
+//        }
+
+
+//        int[] numbers = new int[6];
+//
+//        numbers[0] = 85;
+//        numbers[1] = 94;
+//        numbers[numbers.length - 1] = 100;
+//
+//        System.out.println(numbers.length);
+//
+//        for (int i = 0 ;i < numbers.length; i++) {
+//            System.out.println(numbers[i]);
+//        }
+
+
+        String[] adlar = {"Gulnur", "Fidan", "Minaye", "Fatime"};
+
+//        for (int i = 0; i < adlar.length; i++) {
+//            adlar[i] = adlar[i].toUpperCase();
+//        }
+
+        for (String element : adlar) {
+            System.out.println(element);
+        }
+
+        System.out.println(adlar[0]);
+
+
+//        System.out.println(adlar.length);
+//        adlar[4] = "sadasvj"; // olmaz
+
+//        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+
+//        int[][] array = new int[3][3];
+//        array[0][0] = 1;
+//        array[0][1] = 2;
+//        array[0][2] = 3;
+//        array[1][0] = 4;
+//        array[1][1] = 5;
+//        array[1][2] = 6;
+//        array[2][0] = 7;
+//        array[2][1] = 8;
+//        array[2][2] = 9;
+
+//        int[][] array = {
+//                {1, 2, 3},
+//                {4, 5, 6},
+//                {7, 8, 9}
+//        };
+//
+//        for (int i = 0; i < array.length; i++) {
+//            for (int j = 0; j < array[i].length; j++) {
+//                System.out.print(array[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+//        System.out.println("\u001B[36m" + "Salam");
+
 
     }
 
